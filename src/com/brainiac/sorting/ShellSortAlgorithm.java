7e5585e42fac985edb19d628aac9580b03467120
Brian Njenga <bkuriakb@gmail.com>
2020-02-06 18:55:52 +0300

    Add shell sort algorithm

---
+++ src/com/brainiac/sorting/ShellSortAlgorithm.java
@@ -0,0 +1,26 @@
+package com.brainiac.sorting;
+
+public class ShellSortAlgorithm {
+    public static  void main(String[] args) {
+        int[] intArray = { 10, 20, 45, -7, -22, 35, 55, 1 };
+
+        for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < intArray.length; i++) {
+                int newElement = intArray[i]; 
+
+                int j = i;
+
+                while (j >= gap && intArray[j - gap] > newElement) {
+                    intArray[j] = intArray[j - gap];
+                    j -= gap;
+                }
+                intArray[j] = newElement;
+            }
+        }
+
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+}
